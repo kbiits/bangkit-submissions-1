3a812d13ecb92ed7f941421302878b2ae4f4781d
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2023-05-07 20:04:36 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.set("trust proxy", true);
+app.use(
+  express.static("public", {
+    index: "index.html",
+  })
+);
+
+app.listen(process.env.PORT || 80);
